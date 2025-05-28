984f479b9646a218f06d13623f32d1207a153784
Andcraft <Andcraftt@gmail.com>
2025-05-28 18:03:21 +0200

    quinto commit

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,4 +1,26 @@
 const express = require('express')
-const route = express.Router()
+const router = express.Router()
+const{
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+} = require('../controllers/goalController')
+
+router.get('/', (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+})
+
+router.post('/', (req, res) => {
+    res.status(200).json({message: 'Set goal'})
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+})
 
 module.exports = router
