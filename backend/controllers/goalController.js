88ce2d077cf03bc2059c5d59e9adff19b72e5d67
Andcraft <Andcraftt@gmail.com>
2025-06-03 20:07:53 +0200

    update goal working but not tested

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -20,15 +20,29 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
+    const goal = await Goal.create({
+        text:req.body.text
+    })
 
-    res.status(200).json({message: 'Set goal'})
+    res.status(200).json({goal})
 })
 
 // Update goal
 // /api/goals/id
 // /Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+
+    res.status(200).json(updateGoal)
 })
 // Delete goal
 // /api/goals/id
