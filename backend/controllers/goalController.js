bbc856c8b9736aa583846fee44453c15ad56a618
Andcraft <Andcraftt@gmail.com>
2025-06-04 16:20:19 +0200

    terminado pero el remove no funciona

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -48,7 +48,17 @@ const updateGoal = asyncHandler(async (req, res) => {
 // /api/goals/id
 // /Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+     
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
