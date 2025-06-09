81b2b29fd9a4fe8f666b872ac926d587f7f35233
Andcraft <Andcraftt@gmail.com>
2025-06-09 19:28:27 +0200

    todo funciona register terminado

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,7 +11,16 @@ export default function Register() {
 
     const  {name, email, password, password2} = formData
     
-    const onChange = () => {}
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+    }
 
     return <>
         <section className='heading'>
@@ -22,12 +31,12 @@ export default function Register() {
         </section>
 
         <section className='form'>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className='form-group'>
                 <input type="text" className="form-control" id="name" name="name" value={name} placeholder='Enter your name' onChange={onChange}/>
                 </div>
                 <div className='form-group'>
-                <input type="text" className="form-control" id="email" name="email" value={email} placeholder='Enter your email' onChange={onChange}/>
+                <input type="email" className="form-control" id="email" name="email" value={email} placeholder='Enter your email' onChange={onChange}/>
                 </div>
                 <div className='form-group'>
                 <input type="password" className="form-control" id="password" name="password" value={password} placeholder='Enter your password' onChange={onChange}/>
