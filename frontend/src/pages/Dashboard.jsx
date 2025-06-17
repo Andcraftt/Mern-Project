0dd6b40eb525cb98ebcbfb6714ba3b035a13d79c
Andcraftt <andcraftt@gmail.com>
2025-06-17 18:41:57 +0200

    ada

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
 import GoalForm from '../components/goalForm'
-import GoalItem from '../components/goalItem'
+import GoalItem from '../components/GoalItem.jsx'
 import Spinner from '../components/spinner'
 import {getGoals, reset} from '../features/goals/goalSlice'
 
